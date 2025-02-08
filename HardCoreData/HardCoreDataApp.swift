3ca2adefd306292640373c806066552788cfb8b4
Doanta Ginting <doantaaloycius@gmail.com>
2025-02-08 23:19:50 +0700

    Initial Commit

---
+++ HardCoreData/HardCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  HardCoreDataApp.swift
+//  HardCoreData
+//
+//  Created by Doanta  on 08/02/25.
+//
+
+import SwiftUI
+
+@main
+struct HardCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
